6693aa2d789ac41535d6ddbbacc90dcf2977f425
Robin Green <robin@designingreen.co.uk>
2024-02-06 10:27:49 +0000

    Allow theme select

--- page-single-scroll.php
+++ page-single-scroll.php
@@ -37,61 +37,76 @@ get_header(); ?>
 
 						<?php if(get_row_layout() == 'section_callout'): ?>
 					    	<section class="block--callout">
-								<div class="container">
-									<div class="row justify-content-center">
-										<div class="col-12 col-md-12">
-											<span><?php the_sub_field('sp_callout'); ?></span>
+					    		<?php if(get_sub_field('sp_theme_select') == 'themelight')  { ?>
+									<div class="mol--theme-light">
+								<?php } elseif(get_sub_field('sp_theme_select') == 'themelight') { ?>
+									<div class="mol--theme-dark">
+								<?php } ?>
+										<div class="mol---block-callout">
+											<div class="container">
+												<div class="row justify-content-center">
+													<div class="col-12 col-md-12">
+														<span><?php the_sub_field('sp_callout'); ?></span>
+													</div>
+												</div>
+											</div>
 										</div>
 									</div>
-								</div>
 							</section>
 						<?php endif; ?>
 
 						<?php if(get_row_layout() == 'section_5050'): ?>
 							<section class="block--5050">
-								<div class="container-fluid">
-									<?php if(get_sub_field('sp_image_position') == 'imageleft' ) { ?>
-
-										<div class="row g-0 d-flex">
-											<div class="col-12 col-lg-6 align-self-center order-1">
-												<div class="d-flex justify-content-end">
-													<figure>
-														<img class="mw-100" src="<?php the_sub_field('sp_image_file'); ?>">
-													</figure>
-												</div>
-											</div>
-											<div class="col-12 col-lg-6 align-self-center order-2">
-												<div class="mol--5050">
-													<?php if(get_sub_field('sp_title')) : ?>
-														<h2><?php the_sub_field('sp_title'); ?></h2>
-													<?php endif; ?>
-													<div>
-														<?php the_sub_field('sp_paragraph'); ?>
+								<?php if(get_sub_field('sp_theme_select') == 'themelight')  { ?>
+									<div class="mol--theme-light">
+								<?php } elseif(get_sub_field('sp_theme_select') == 'themelight') { ?>
+									<div class="mol--theme-dark">
+								<?php } ?>
+									<div class="container-fluid">
+										<?php if(get_sub_field('sp_image_position') == 'imageleft' ) { ?>
+											
+												<div class="row g-0 d-flex">
+													<div class="col-12 col-lg-6 align-self-center order-1">
+														<div class="d-flex justify-content-end">
+															<figure>
+																<img class="mw-100" src="<?php the_sub_field('sp_image_file'); ?>">
+															</figure>
+														</div>
+													</div>
+													<div class="col-12 col-lg-6 align-self-center order-2">
+														<div class="mol--5050">
+															<?php if(get_sub_field('sp_title')) : ?>
+																<h2><?php the_sub_field('sp_title'); ?></h2>
+															<?php endif; ?>
+															<div>
+																<?php the_sub_field('sp_paragraph'); ?>
+															</div>
+														</div>
 													</div>
 												</div>
 											</div>
-										</div>
-									<?php } elseif(get_sub_field('sp_image_position') == 'imageright') { ?>
-										<div class="row g-0 d-flex">
-											<div class="col-12 col-lg-6 align-self-center order-2 order-lg-1">
-												<div class="mol--5050">
-													<?php if(get_sub_field('sp_title')) : ?>
-														<h2><?php the_sub_field('sp_title'); ?></h2>
-													<?php endif; ?>
-													<div>
-														<?php the_sub_field('sp_paragraph'); ?>
+										<?php } elseif(get_sub_field('sp_image_position') == 'imageright') { ?>
+											<div class="row g-0 d-flex">
+												<div class="col-12 col-lg-6 align-self-center order-2 order-lg-1">
+													<div class="mol--5050">
+														<?php if(get_sub_field('sp_title')) : ?>
+															<h2><?php the_sub_field('sp_title'); ?></h2>
+														<?php endif; ?>
+														<div>
+															<?php the_sub_field('sp_paragraph'); ?>
+														</div>
+													</div>
+												</div>
+												<div class="col-12 col-lg-6 align-self-center order-1 orderlg-2">
+													<div class="d-flex">
+														<figure>
+															<img class="mw-100" src="<?php the_sub_field('sp_image_file'); ?>">
+														</figure>
 													</div>
 												</div>
 											</div>
-											<div class="col-12 col-lg-6 align-self-center order-1 orderlg-2">
-												<div class="d-flex">
-													<figure>
-														<img class="mw-100" src="<?php the_sub_field('sp_image_file'); ?>">
-													</figure>
-												</div>
-											</div>
-										</div>
-									<?php } ?>
+										<?php } ?>
+									</div>
 								</div>
 							</section>
 						<?php endif; ?>
